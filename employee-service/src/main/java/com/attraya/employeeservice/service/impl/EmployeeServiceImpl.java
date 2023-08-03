312578aef166d1fb0e1fd3294d5aecf7ee1658fc
Attraya Das <attrayaghoshdas@gmail.com>
2023-08-03 22:23:58 +0530

    EmployeeService - Microservices Communication using WebClient

--- employee-service/src/main/java/com/attraya/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/attraya/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -12,6 +12,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -19,7 +20,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
-    private RestTemplate restTemplate;
+//    private RestTemplate restTemplate;
+
+    private WebClient webClient;
 
     private ModelMapper mapper;
 
@@ -40,10 +43,15 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee", "id", employeeId));
 
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
-                DepartmentDto.class);
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+//                DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = responseEntity.getBody();
 
-        DepartmentDto departmentDto = responseEntity.getBody();
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve().bodyToMono(DepartmentDto.class)
+                .block();
 
         EmployeeDto employeeDto = mapper.map(employee, EmployeeDto.class);
 
