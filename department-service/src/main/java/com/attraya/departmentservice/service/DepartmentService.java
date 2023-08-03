6e3793975c072983ec5882a3c773902c83542cd6
Attraya Das <attrayaghoshdas@gmail.com>
2023-08-03 17:33:47 +0530

    DepartmentService - Build Save & Get Department REST API

---
+++ department-service/src/main/java/com/attraya/departmentservice/service/DepartmentService.java
@@ -0,0 +1,8 @@
+package com.attraya.departmentservice.service;
+
+import com.attraya.departmentservice.dto.DepartmentDto;
+
+public interface DepartmentService {
+    DepartmentDto saveDepartment(DepartmentDto departmentDto);
+    DepartmentDto getDepartmentByCode(String code);
+}
