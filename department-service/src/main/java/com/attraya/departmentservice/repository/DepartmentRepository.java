fc787862f4647a53eac2716f75a1723d125db362
Attraya Das <attrayaghoshdas@gmail.com>
2023-08-03 15:14:55 +0530

    DepartmentService - Configure MySQL Database, Create Department JPA Entity & JPA Repository

---
+++ department-service/src/main/java/com/attraya/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.attraya.departmentservice.repository;
+
+import com.attraya.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
