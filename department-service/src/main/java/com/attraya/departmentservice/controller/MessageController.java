a0287a67ea0d1b97e1293a92d98bf2349858308d
Attraya Das <attrayaghoshdas@gmail.com>
2023-08-09 20:36:03 +0530

    Refresh Use case - No Restart Required After Config Changes using @RefreshScope & /actuator/refresh

---
+++ department-service/src/main/java/com/attraya/departmentservice/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.attraya.departmentservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope // After updating the config-server-repo -> POST: http://localhost:8080/actuator/refresh
+@RestController
+public class MessageController {
+
+    @Value("${spring.boot.message}")
+    private String message;
+
+    @GetMapping("message")
+    public String message(){
+        return message;
+    }
+}
