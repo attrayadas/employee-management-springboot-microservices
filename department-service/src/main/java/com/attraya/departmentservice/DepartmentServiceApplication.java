93571ba2f4ae6c91d7a19a007cfad25f1740e7a8
Attraya Das <attrayaghoshdas@gmail.com>
2023-08-13 01:28:30 +0530

    Department-Service - REST API Documentation

--- department-service/src/main/java/com/attraya/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/attraya/departmentservice/DepartmentServiceApplication.java
@@ -1,10 +1,32 @@
 package com.attraya.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service REST APIs",
+				description = "Department Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Attraya",
+						email = "attrayaghoshdas@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department-Service Doc"
+		)
+)
 @SpringBootApplication // @EnableEurekaClient - This annotation was removed in spring cloud 2022.0.0 and provided auto-configuration
 public class DepartmentServiceApplication {
 
