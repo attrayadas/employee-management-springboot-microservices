71c99631475a879c9085414a9853ecab2b3f6d24
Attraya Das <attrayaghoshdas@gmail.com>
2023-08-13 12:29:30 +0530

    Employee-Service - REST API Documentation

--- employee-service/src/main/java/com/attraya/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/attraya/employeeservice/EmployeeServiceApplication.java
@@ -1,5 +1,10 @@
 package com.attraya.employeeservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +13,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Employee Service REST APIs",
+				description = "Employee Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Attraya",
+						email = "attrayaghoshdas@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Employee-Service Doc"
+		)
+)
 @SpringBootApplication
 @EnableFeignClients // @EnableEurekaClient - This annotation was removed in spring cloud 2022.0.0 and provided auto-configuration
 public class EmployeeServiceApplication {
--- employee-service/src/main/java/com/attraya/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/attraya/employeeservice/controller/EmployeeController.java
@@ -3,11 +3,18 @@ package com.attraya.employeeservice.controller;
 import com.attraya.employeeservice.dto.APIResponseDto;
 import com.attraya.employeeservice.dto.EmployeeDto;
 import com.attraya.employeeservice.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Employee Service - Employee Controller",
+        description = "Employee Controller Exposes REST APIs for Employee-Service"
+)
 @RestController
 @RequestMapping("api/employees")
 @AllArgsConstructor
@@ -15,6 +22,14 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
+    @Operation(
+            summary = "Save Employee REST API",
+            description = "Save Employee REST API is used to save employee object in the database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     // build Save employee REST API
     @PostMapping
     public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto){
@@ -22,6 +37,14 @@ public class EmployeeController {
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Employee REST API",
+            description = "Get Employee REST API is used to get a single employee object from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 OK"
+    )
     // build Get employee REST API
     @GetMapping("{id}")
     public ResponseEntity<APIResponseDto> getEmployee(@PathVariable("id") Long employeeId){
